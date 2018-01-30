4420b49edc4b1472f54204408c8fc33f2776f0b9
Paul REBOH <reboh.p@gmail.com>
2018-01-30 20:22:54 +0100

    update constant logic and test

--- tests/constant.test.js
+++ tests/constant.test.js
@@ -2,7 +2,13 @@ import _ from 'lodash';
 import Constant from '../src/misc/constant';
 
 describe('Defining a Constant', () => {
-  let scope = Constant('test1', {'n1': true, 'n2': 'someValue'});
+  let scope = Constant('test1', {
+    'n1': true,
+    'n2': 'someValue',
+    'n3': {
+      'n3-1': true
+    }
+  });
 
   it('returns a function', () => {
     expect(_.isFunction(scope)).toBe(true);
@@ -24,17 +30,53 @@ describe('Defining a Constant', () => {
   });
 });
 
+describe('Existing constant', () => {
+
+  it('can be extended at the end', () => {
+    Constant('test1.n3.n3-1', {
+      'n3-1-1': true,
+      'n3-1-2': {
+        'n3-1-2-1': true,
+        'n3-1-2-2': true,
+        'n3-1-2-3': true
+      }
+    });
+    let constant = Constant('test1.n3.n3-1.n3-1-2.n3-1-2-1');
+    expect(constant).toEqual('TEST1.N3.N3-1.N3-1-2.N3-1-2-1');
+  });
+
+  it('can be extended and merged at a node', () => {
+    Constant('test1.n3', {
+      'n3-2': {
+        'n3-2-1': true,
+        'n3-2-2': true,
+        'n3-2-3': true
+      }
+    });
+    let preset = Constant('test1.n3.n3-1.n3-1-2.n3-1-2-1');
+    let newset = Constant('test1.n3.n3-2.n3-2-1');
+    expect(preset).toEqual('TEST1.N3.N3-1.N3-1-2.N3-1-2-1');
+    expect(newset).toEqual('TEST1.N3.N3-2.N3-2-1');
+  });
+
+});
+
 describe('Defining a Constant with an Array', () => {
-  let scope = Constant('test2', {
+  let scope = Constant('test2', [{
     'n1': [
       'n1-1',
       {'n1-2': ['n1-2-1','n1-2-2']}
     ]
-  });
+  }, 'n2']);
 
-  it('turns it into an object', () => {
+  it('turns it into an object (case1)', () => {
     let constant = Constant('test2.n1.n1-2.n1-2-1');
     expect(constant).toEqual('TEST2.N1.N1-2.N1-2-1');
+  });
+
+  it('turns it into an object (case2)', () => {
+    let constant = Constant('test2.n2');
+    expect(constant).toEqual('TEST2.N2');
   });
 });
 
@@ -58,7 +100,6 @@ describe('Accessing a Constant', () => {
     let constant = Constant('test3.n2.n2-1');
     expect(constant).toEqual('TEST3.N2.N2-1');
   });
-
 });
 
 describe('Access a scoped path with "."', () => {
@@ -87,7 +128,6 @@ describe('Accessing all constants', () => {
   it('returns a plain object', () => {
     expect(_.isPlainObject(constants)).toEqual(true);
   });
-
 });
 
 
