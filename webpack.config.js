03b973e2f721139f318a8c934025b4361c5f94d0
Paul REBOH <reboh.p@gmail.com>
2018-06-12 15:35:29 +0200

    updates webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -5,38 +6,40 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const __DEV__ = process.env.NODE_ENV !== 'production';
 const ___WEBPACK_ANALYZE__ = process.env.___WEBPACK_ANALYZE__;
 
-const webpackConfig = {
-  entry: './src/index.js',
-  mode: __DEV__ ? 'development' : 'production',
-  output: {
-    path: path.resolve(__dirname, 'dist/'),
-    filename: 'kawax.js',
-    library: 'kawax-js',
-    libraryTarget: 'umd',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-      }
+module.exports = __ENV__ => {
+  const webpackConfig = {
+    entry: './src/index.js',
+    mode: __DEV__ ? 'development' : 'production',
+    output: {
+      path: path.resolve(__dirname, 'dist/'),
+      filename: 'kawax.js',
+      library: 'kawax-js',
+      libraryTarget: 'umd',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+        }
+      ]
+    },
+    node: {
+      net: 'empty',
+      fs: 'empty',
+      tls: 'empty'
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __DEV__: (__ENV__ !== 'production')
+      })
     ]
-  },
-  node: {
-    net: 'empty',
-    fs: 'empty',
-    tls: 'empty'
-  },
-  plugins: []
+  };
+
+  if (__DEV__ && ___WEBPACK_ANALYZE__) {
+    webpackConfig.plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return webpackConfig;
 };
-
-if (!__DEV__) {
-  webpackConfig.plugins.push(new LodashModuleReplacementPlugin());
-}
-
-if (__DEV__ && ___WEBPACK_ANALYZE__) {
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin());
-}
-
-module.exports = webpackConfig;
