dadb8a44cbd5970cfe41388010e860f39e74b8e8
Paul REBOH <reboh.p@gmail.com>
2018-10-12 05:43:38 +0200

    Resource improvements

--- src/Resource.js
+++ src/Resource.js
@@ -8,10 +8,11 @@ class Resource extends Smart {
 
   defaults(options) { return options; }
 
-  context = () => {};
-
-  _resolveContext(resolver) {
+  _resolveContext(resolver, options) {
     return {
+      ...options,
+      schema: resolver('schema') || {},
+      mock: resolver('mock', false) || false,
       path: resolver('path') || log.error('Resource path is undefined'),
       method: resolver('method') || log.error('Resource method is undefined'),
       baseUri: resolver('baseUri') || '/',
@@ -20,16 +21,12 @@ class Resource extends Smart {
       credentials: resolver('credentials') || 'same-origin',
       responseType: resolver('responseType') || 'json',
       collection: resolver('collection') || false,
-      serializeRequest: resolver('serializeRequest') || true,
-      requestParser: resolver('requestParser', false) || ((payload) => payload),
+      requestParser: resolver('requestParser', false) || false,
       responseParser: resolver('responseParser', false) || ((response, body) => body),
       entityParser: resolver('entityParser', false) || false,
       errorParser: resolver('errorParser', false) || ((payload) => payload),
       requestTransform: resolver('requestTransform') === false ? false : _.snakeCase,
       responseTransform: resolver('responseTransform') === false ? false : _.camelCase,
-      options: resolver('options') || {},
-      mock: resolver('mock') || false,
-      ...this.context() || {},
     };
   }
 
@@ -42,11 +39,17 @@ class Resource extends Smart {
     }
   );
 
-  define(options) {
-    return (payload) => {
-      const resolver = this._getResolver(options, payload);
-      const context = this._resolveContext(resolver);
-      return ResourceCall.export(context)(payload);
+  _getContext(options, payload) {
+    const resolver = this._getResolver(options, payload);
+    return this._resolveContext(resolver, options);
+  }
+
+  define(baseOptions) {
+    return (payload, callOptions) => {
+      const options = { ...baseOptions, ...callOptions };
+      const context = this._getContext(options, payload);
+      const resourceCall = ResourceCall.export(context);
+      return resourceCall(payload);
     };
   }
 
